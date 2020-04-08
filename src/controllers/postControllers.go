eff564c0950663413e06d48f65808fa1c69288b3
Varun Gupta <varun.gupta3@honeywell.com>
2020-04-09 04:14:19 +0530

    Added get post for user id and post id

--- src/controllers/postControllers.go
+++ src/controllers/postControllers.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"golang.org/x/net/context"
+	"google.golang.org/api/iterator"
 	"ivar-go/src/client"
 	"ivar-go/src/models"
 	"log"
@@ -12,28 +14,65 @@ import (
 
 func GetPostsByUserId(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
+
+	firestore, _ := client.GetFirestoreClient()
+	defer firestore.Close()
+
+	doc := firestore.Collection("users").Doc(vars["userId"]).Collection("posts")
+	iter := doc.Documents(context.Background())
+	defer iter.Stop()
+
+	var posts []models.Post
+
+	for {
+		var post models.Post
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error iterating: %s", err)
+		}
+
+		jsonString, _ := json.Marshal(doc.Data())
+		err = json.Unmarshal(jsonString, &post)
+		if err != nil {
+			log.Fatalf("Error unmarshalling to json: %s", err)
+		}
+		post.ID = doc.Ref.ID
+		posts = append(posts, post)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	err := json.NewEncoder(w).Encode(posts)
+	if err != nil {
+		log.Fatalf("Error encoding data: %s", err)
+	}
+}
+
+func GetPostByPostId(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
 	ctx := context.Background()
 
 	firestore, _ := client.GetFirestoreClient()
 	defer firestore.Close()
 
-	doc, err := firestore.Collection("users").Doc(vars["userId"]).Get(ctx)
+	doc, err := firestore.Collection(fmt.Sprintf("users/%s/posts", vars["userId"])).Doc(vars["postId"]).Get(ctx)
 	if err != nil {
-		log.Fatalf("Error fetching data from firestore: %s", err)
+		log.Fatalf("Unable to fetch data: %s", err)
 	}
 
-	postsData, _ := doc.DataAt("posts")
+	var post models.Post
 
-	var posts []models.Post
-
-	jsonString, _ := json.Marshal(postsData)
-	err = json.Unmarshal(jsonString, &posts)
+	jsonString, _ := json.Marshal(doc.Data())
+	err = json.Unmarshal(jsonString, &post)
 	if err != nil {
 		log.Fatalf("Error unmarshalling to json: %s", err)
 	}
+	post.ID = doc.Ref.ID
 
 	w.WriteHeader(http.StatusOK)
-	err = json.NewEncoder(w).Encode(posts)
+	err = json.NewEncoder(w).Encode(post)
 	if err != nil {
 		log.Fatalf("Error encoding data: %s", err)
 	}
--- src/web/web.go
+++ src/web/web.go
@@ -13,6 +13,7 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/users", controllers.UsersController)
 	router.HandleFunc("/posts/{userId}", controllers.GetPostsByUserId)
+	router.HandleFunc("/post/{userId}/{postId}", controllers.GetPostByPostId)
 	fmt.Println("IVAR-Go listening at port: 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
